b985b9d974fc4f874c7501015881b95296c8f261
Marcus Munoz <munoz.marcus05@gmail.com>
2022-07-27 12:31:08 -0500

    mini exercise 2 favorite city

--- src/main/java/com/example/codeup/springblog/ExampleViewController.java
+++ src/main/java/com/example/codeup/springblog/ExampleViewController.java
@@ -4,6 +4,8 @@ package com.example.codeup.springblog;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ExampleViewController {
@@ -11,5 +13,27 @@ public class ExampleViewController {
     public String returnHelloWorld(Model vModel){
         vModel.addAttribute("name", "Marcus!");
         return "hello-world";
+    }
+
+    @GetMapping("/search")
+    public String returnSearchForm(){
+        return "search";
+    }
+
+    @PostMapping("/search")
+    public String returnSearchResults(@RequestParam String query, Model vModel){
+        vModel.addAttribute("search", query);
+        return "search-results";
+    }
+
+    @GetMapping("/city")
+    public String returnCityForm(){
+        return "city";
+    }
+
+    @PostMapping("/city")
+    public String returnCityResults(@RequestParam String location, Model vModel){
+        vModel.addAttribute("city", location);
+        return "city-results";
     }
 }
